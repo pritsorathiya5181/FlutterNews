dbb75c0cd60fb857553c84f892b2167a098ac685
prit <prit.sorathiya@gmail.com>
2020-06-18 13:20:18 +0530

    "complete category news"

---
+++ lib/helper/news.dart
@@ -0,0 +1,9 @@
+import 'package:FlutterNews/models/article_model.dart';
+
+class News {
+  List<ArticleModel> news = [];
+
+  String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d6c0b757c2d8409b9cbd50910e6d9170";
+  
+
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+import 'package:FlutterNews/models/category_model.dart';
+import 'package:FlutterNews/helper/data.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,6 +8,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +24,8 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             Text('Flutter'),
             Text(
-              'News',style: TextStyle( color: Colors.blue ),
+              'News',
+              style: TextStyle(color: Colors.blue),
             )
           ],
         ),
@@ -23,7 +33,71 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: Container(
-        
+        child: Column(
+          children: <Widget>[
+            Container(
+              // padding: EdgeInsets.symmetric(horizontal: 16.0),
+              height: 70.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    imageUrl: categories[index].imageUrl,
+                    categoryName: categories[index].categorieName,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
+
+  const CategoryTile({this.imageUrl, this.categoryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(left: 13.0, right: 3.0),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6.0),
+              child: Image.network(
+                imageUrl,
+                width: 120.0,
+                height: 60.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 120.0,
+              height: 60.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6.0),
+                color: Colors.black26,
+              ),
+              child: Text(
+                categoryName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
