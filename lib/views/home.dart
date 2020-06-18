79145cefe71ee7680595fec5b6fa27e23e7ad453
prit <prit.sorathiya@gmail.com>
2020-06-18 11:47:03 +0530

    "initial Commit"

---
+++ lib/views/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Flutter'),
+            Text(
+              'News',style: TextStyle( color: Colors.blue ),
+            )
+          ],
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
